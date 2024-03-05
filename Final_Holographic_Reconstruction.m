3dab84cf4ea44038462367f1f4138a391db51aba
AdityaMuppala <142175661+AdityaMuppala@users.noreply.github.com>
2024-03-05 16:00:35 -0500

    Update Final_Holographic_Reconstruction.m

--- Final_Holographic_Reconstruction.m
+++ Final_Holographic_Reconstruction.m
@@ -1,6 +1,6 @@
 %-- Code written by Aditya Varma Muppala for the paper titled: Fast-Fourier Time-Domain SAR Reconstruction for
 %-- Millimeter-Wave FMCW 3-D Imaging. Last edited on 03/05/2024. 
-%-- The data can be downloaded from google drive using the following link:
+%-- The data can be downloaded from google drive using the following link: https://drive.google.com/file/d/1AnGdp6I1WtOdJQiCnRePAfU8fW8nTHk3/view?usp=sharing
 %-- Extract the zip file into a folder titled "Imaging_raw_data" and place in the same path as the MATLAB codes.
 
 clc
